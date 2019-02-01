78225727fb8fe99c630e34de6128a045400b7175
= <=>
2019-02-01 15:41:41 -0200

    import layout and add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import VotingContract from "./contracts/Voting.json";
 import getWeb3 from "./utils/getWeb3";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import "./App.css";
-import WrongNetwork from "./error/WrongNetwork";
+import WrongNetwork from "./not-found/WrongNetwork";
+import Navbar from "./layout/Navbar";
+import Landing from "./layout/Landing";
+import Footer from "./layout/Footer";
 
 class App extends Component {
   state = { proposals: [], web3: null, accounts: null, contract: null, correctNetwork: true };
@@ -62,19 +66,14 @@ class App extends Component {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
+      <Router>
+        <div className='container-fluid'>
+          <Navbar/>
+          <Route exact path="/" component={Landing}/>
+            <div>The stored value is: {this.state.storageValue}</div>
+          <Footer/>
+        </div>
+      </Router>
     );
   }
 }
