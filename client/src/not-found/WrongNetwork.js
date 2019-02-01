5be5f13881f8725b616e30842e53e0a79b38c7ee
= <=>
2019-02-01 15:40:45 -0200

    change wrongNetwork directory

---
+++ client/src/not-found/WrongNetwork.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class WrongNetwork extends Component {
+    
+    render() {
+        return (
+            <div>
+                Wrong WrongNetwork :(
+            </div>
+        )
+    } 
+}
+
+export default WrongNetwork;
