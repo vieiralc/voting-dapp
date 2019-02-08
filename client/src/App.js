3bafc80815c2bac00fff570e7fd94972f026169f
= <=>
2019-02-08 16:28:25 -0200

    rm runexample func

--- client/src/App.js
+++ client/src/App.js
@@ -6,72 +6,58 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import WrongNetwork from "./not-found/WrongNetwork";
 import Navbar from "./layout/Navbar";
 import Landing from "./layout/Landing";
-import Footer from "./layout/Footer";
 import Spinner from "./common/Spinner";
 
 class App extends Component {
   state = { proposals: [], web3: null, accounts: null, contract: null, correctNetwork: true };
 
-  componentDidMount = async () => {
+  componentWillMount = async () => {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
+      web3.eth.defaultAccount = accounts[0];
+      
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = VotingContract.networks[networkId];
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      
-      const deployedNetwork = VotingContract.networks[networkId];
       const instance = new web3.eth.Contract(
         VotingContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
-      // add a check for network ID before continue
-      console.log('​App -> componentDidMount -> networkId', networkId);
-      if (networkId === 5777) {
+      if (networkId === 4447) {
         // Set web3, accounts, and contract to the state, and then proceed with an
         // example of interacting with the contract's methods.
-        this.setState({ web3, accounts, contract: instance }, this.runExample);
+        this.setState({ web3, accounts, contract: instance });
       } else {
         this.setState({ correctNetwork: false })
       }
       
     } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Please, install metamask in order to use this dapp.`,
-      );
+      alert(`Please, install metamask in order to use this dapp.`,);
       console.error(error);
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    const proposals = await contract.methods.getAllProposals().call({ from: accounts[0] });
-    console.log(proposals)
-    // Update state with the result.
-    this.setState({ proposals: proposals });
-  };
-
   render() {
+    
     if (!this.state.correctNetwork) {
       return <WrongNetwork/>;
     }
+
     if (!this.state.web3) {
       return <Spinner/>;
     }
+    
     return (
       <Router>
         <div>
           <Navbar/>
-          <Route exact path="/" component={Landing}/>
-            <div>The stored value is: {this.state.storageValue}</div>
-          <Footer/>
+          <Route exact path="/" render={() => <Landing contract={this.state.contract} accounts={this.state.accounts}/>}/>
         </div>
       </Router>
     );
