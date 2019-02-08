a165f0fe42d943b39e796e803168e7b7d257faeb
= <=>
2019-02-08 16:52:24 -0200

    add css for vote button

--- client/src/layout/Card.js
+++ client/src/layout/Card.js
@@ -8,27 +8,42 @@ class Card extends Component {
         super();
 
         this.state = {
-            bookmark: false
+            bookmark: false,
+            votepositive: false,
+            votenagative: false
         };
     };
 
     componentDidMount() {
         this.props.contract.events.Voted({fromBlock: 'latest'})
             .on('data', event => {
-                console.log(event.returnValues[2]);
-				$(`#positive0`).html(event.returnValues[2]);
-                $(`#negative0`).html(event.returnValues[3]);
+				$(`#positive${event.returnValues[0]}`).html(event.returnValues[2]);
+                $(`#negative${event.returnValues[0]}`).html(event.returnValues[3]);
             });
     }
 
     bookmarkProposal() {
-      this.setState({bookmark: !this.state.bookmark})  
+      this.setState({bookmark: !this.state.bookmark});
+    };
+
+    votePositive() {
+        if (!this.state.votenagative && !this.state.votepositive) {
+            this.setState({votepositive: !this.state.votepositive});
+        }
+    };
+
+    voteNegative() {
+        if (!this.state.votepositive && !this.state.votenagative) {
+            this.setState({votenagative: !this.state.votenagative});
+        }
     };
 
     render() {
 
         const { proposal } = this.props;
         let bookmarkclass = this.state.bookmark ? 'bookmarked' : 'unmarked';
+        let votepositiveclass = this.state.votepositive ? 'positiveVotes' : 'not-voted';
+        let votenegativeclass = this.state.votenagative ? 'negativeVotes' : 'not-voted';
 
         return (
             <div className="card text-white bg-dark mb-3">
@@ -59,7 +74,7 @@ class Card extends Component {
                     <div className="row">
                         <div className="col-md-4 col-sm-4 col-4 text-left">
                             <span id={`positive${proposal.id}`} className="badge badge-success"> {proposal.positiveVotes} </span> &nbsp;
-                            <i className="fas fa-thumbs-up"> </i>
+                            <i className={`fas fa-thumbs-up ${votepositiveclass}`} onClick={this.votePositive.bind(this)}> </i>
                         </div>  
 
                         <div className="col-md-4 col-sm-4 col-4 text-center">
@@ -68,7 +83,7 @@ class Card extends Component {
 
                         <div className="col-md-4 col-sm-4 col-4 text-right">
                             <span id={`negative${proposal.id}`} className="badge badge-danger"> {proposal.negativeVotes} </span> &nbsp;
-                            <i className="fas fa-thumbs-down"> </i>
+                            <i className={`fas fa-thumbs-down ${votenegativeclass}`} onClick={this.voteNegative.bind(this)}> </i>
                         </div>
                     </div>
                     
