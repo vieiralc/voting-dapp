31fb05ee06f8c433af63fa4d5a5b6656cc0f1270
= <=>
2019-02-20 14:47:12 -0300

    deploy contract

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const HDwalletProvider = require('truffle-hdwallet-provider');
 const path = require("path");
 
 module.exports = {
@@ -5,10 +7,29 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    "development": {
+    
+    development: {
       host: "localhost",
-      port: "8545",
+      port: "9545",
       network_id: "*"
+    },
+    
+    ropsten: {
+      provider: function() {
+        return new HDwalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.infuraKey}`
+        );
+      },
+      gasPrice: 25000000000,
+      network_id: 3
+    },
+    
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
 };
